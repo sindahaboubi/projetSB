abd3caf7724fb0896c268337773d855e385d897f
Sinda <sindahaboubi104@gmail.com>
2024-05-09 22:22:13 +0100

    first commit

---
+++ src/main/java/projet1/Entites/Inscription.java
@@ -0,0 +1,23 @@
+package projet1.Entites;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Inscription implements Serializable {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long numInscription;
+    private int numSemaine;
+    @ManyToOne
+    private Skieur skieur;
+    @ManyToOne
+    private Cours cours;
+}
